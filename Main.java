9534c0f958d186a70f49e38fee4f7c2f25fea7bd
inammar <inam999@gmail.com>
2025-02-07 16:48:33 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://magento.softwaretestingboard.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.linkText("Create an Account")).click();
+    }
+}
